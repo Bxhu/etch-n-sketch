e3f071edfc6903967ee96a3c4d41495a47a0093f
Ben Hu <benjamin.x.hu@gmail.com>
2017-06-03 12:31:38 -0400

    Changed to percentage based grid

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
 $(document).ready(function() {
 	function createGrid(n) {
-		var m = 640/n;
+		// var m = (n/640) * 100;
+		var m = (1/n) * 100;
 
-		for (var i = 0; i < n; i++) {
-			// var rowId = "row" + i;
-			// var row = $('<div id="' + rowId + '" class="row"></div>');
-			var row = $('<div class="row"></div>');
-			$("#container").append(row);
+		// for (var i = 0; i < n; i++) {
+		// 	// var rowId = "row" + i;
+		// 	// var row = $('<div id="' + rowId + '" class="row"></div>');
+		// 	var row = $('<div class="row"></div>');
+		// 	$("#container").append(row);
+		// }
+
+		// $(".row").css({height: m + ""});
+
+		// for (var i = 0; i < n; i++) {
+		// 	// var colId = "col" + i;
+		// 	// var col = $('<div id="' + colId + '" class="col"></div>');
+		// 	var col = $('<div class="col"></div>');
+		// 	$(".row").append(col);
+		// }
+
+		// $(".col").css({width: m + "px"});
+		// $(".col").css({height: m + "px"});
+
+
+
+
+		for (var i = 0; i < n*n; i++) {
+			var cell = $('<div class="cell"></div>');
+			cell.css({width: m + "%"});
+			cell.css({height: m + "%"});
+			$("#container").append(cell);
 		}
-
-		$(".row").css({height: m + "px"});
-
-		for (var i = 0; i < n; i++) {
-			// var colId = "col" + i;
-			// var col = $('<div id="' + colId + '" class="col"></div>');
-			var col = $('<div class="col"></div>');
-			$(".row").append(col);
-		}
-
-		$(".col").css({width: (m-2) + "px"});
-		$(".col").css({height: (m-2) + "px"});
 	}
 
 	createGrid(16);
 
-	$("#container").on('mouseenter', '.col',
+	$("#container").on('mouseenter', '.cell',
 		function() {
 			$(this).addClass("visited");
 		}
